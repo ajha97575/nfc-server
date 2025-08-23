9e78edc0d209d450684b46c9f2034146be4c5fa8
Abhishek05012004 <138971674+Abhishek05012004@users.noreply.github.com>
2025-08-23 13:50:29 +0530

    23rd August

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
@@ -34,7 +34,7 @@ const productSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-  },
-)
+  }
+);
 
-module.exports = mongoose.model("Product", productSchema)
+module.exports = mongoose.model("Product", productSchema);
